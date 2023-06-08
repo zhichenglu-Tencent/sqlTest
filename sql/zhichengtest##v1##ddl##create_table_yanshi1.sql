144d46a62dacc2a757cb9675597bed92e0fc9b8d
zhichenglu <zhichenglu@tencent.com>
2023-06-08 17:26:54 +0800

    测试2

--- sql/zhichengtest##v1##ddl##create_table_yanshi1.sql
+++ sql/zhichengtest##v1##ddl##create_table_yanshi1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `yanshi3` (
                           `id` INT NOT NULL AUTO_INCREMENT COMMENT 'string',
-                          `username` VARCHAR(50) NOT NULL,
+                          `username` VARCHAR(32) NOT NULL,
                           `password` VARCHAR(255) NOT NULL,
                           `email` VARCHAR(255) NOT NULL,
                           `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
